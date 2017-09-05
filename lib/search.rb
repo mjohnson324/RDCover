3b83d39ccbeb61a6ef0bcfc15dd903402c41cbc6
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-05 17:55:24 -0400

    Add filter functionality to SQL wrappe class

--- lib/search.rb
+++ lib/search.rb
@@ -3,6 +3,25 @@ require_relative 'sql_object'
 
 module Search
   def where(params)
+    where_line = param_filters(params)
+    results = DBConnection.execute(<<-SQL, *params.values)
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    WHERE
+      #{where_line}
+    SQL
+    self.parse_all(results)
+  end
+
+  private
+
+  def param_filters(params)
+    column_names = params.keys
+    column_names.map do |column|
+      "#{column} = ?"
+    end.join(" AND ")
   end
 end
 
