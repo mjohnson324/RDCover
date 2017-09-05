4a3c7a7b1ef7fa0c73c1ac81e6b0fedd5affb93a
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-05 15:39:30 -0400

    Outline object wrapper class, define table naming methods

---
+++ README.md
@@ -0,0 +1,6 @@
+# RDCover
+---
+
+RDCover, or Ruby Data Cover is a library for interfacing with SQL databases.
+It makes the programmer's job of querying a database much less tedious with
+Ruby methods, allowing easy lookup with little configuration.
--- lib/sql_object.rb
+++ lib/sql_object.rb
@@ -1,8 +1,31 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
 
-# Connect object model to database
 class SQLObject
   def initialize
+  end
+
+  def self.table_name
+    stringified_class_name = self.to_s
+    @table_name ||= stringified_class_name.tableize
+  end
+
+  def self.table_name=(name)
+    @table_name ||= name
+  end
+
+  def self.all
+  end
+
+  def self.find
+  end
+
+  def insert
+  end
+
+  def save
+  end
+
+  def update
   end
 end
