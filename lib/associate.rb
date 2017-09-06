75a0bdb1c050f8db0dc3104dda152fb503b867ca
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-05 21:20:14 -0400

    Define options for has_many and belongs_to

--- lib/associate.rb
+++ lib/associate.rb
@@ -16,17 +16,37 @@ class BelongsToOptions < AssociationOptions
   def initialize(name, options = {})
     defaults = { foreign_key: "#{name}_id".to_sym,
                  class_name: name.to_s.camelcase,
-                 primary_key: :id}
-
+                 primary_key: :id }
     options = defaults.merge(options)
-
     options.each_key { |key| send("#{key}=", options[key]) }
+  end
 end
 
 class HasManyOptions < AssociationOptions
+  def initialize(name, self_class_name, options = {})
+    name = name.to_s
+    defaults = { foreign_key: "#{self_class_name}_id".underscore.to_sym,
+                 class_name: name.singularize.camelcase,
+                 primary_key: :id }
+    options = defaults.merge(options)
+    options.each_key { |key| send("#{key}=", options[key]) }
+  end
 end
 
 module Associates
+  def belongs_to(name, options = {})
+    belong_options = BelongsToOptions.new(name, options)
+    define_method(name) do
+
+    end
+  end
+
+  def has_many(name, options = {})
+    owning_options = HasManyOptions.new(name, options)
+    define_method(name) do
+      
+    end
+  end
 end
 
 class SQLObject
