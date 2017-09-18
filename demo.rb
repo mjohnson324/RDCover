3b9e44acff0f7a4a73489fdb41138105d7b50d0b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-17 21:56:41 -0400

    Set up demo classes and associations

--- demo.rb
+++ demo.rb
@@ -1,1 +1,19 @@
 require_relative 'rdcover'
+
+# pets: id, name, owner_id
+# characters, id, fname, lname, house_id
+# homes: id, address
+
+class Pet < SQLObject
+  belongs_to :character
+  has_one_through :house, :character, :home
+end
+
+class Character < SQLObject
+  has_many :pets
+  belongs_to :home
+end
+
+class Home < SQLObject
+  has_many :characters
+end
