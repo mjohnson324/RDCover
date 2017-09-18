0d74187821d93f557024183a40ce592bfae87ba3
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-17 20:09:22 -0400

    Define has_one_through join association

--- lib/associate.rb
+++ lib/associate.rb
@@ -41,7 +41,7 @@ module Associates
       foreign_key = self.send(belong_options.foreign_key)
       owner_class = belong_options.model_class
       primary_key = belong_options.primary_key
-      owner_class.where(primary_key => foreign_key)
+      owner_class.where(primary_key => foreign_key).first
     end
   end
 
@@ -60,7 +60,30 @@ module Associates
   end
 
   def has_one_through(name, through_name, source_name)
-    
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+      through_table = through_options.table_name
+      through_primary_key = through_options.primary_key
+      through_foreign_key = through_options.foreign_key
+      source_table = source_options.table_name
+      source_primary_key = source_options.primary_key
+      source_foreign_key = source_options.foreign_key
+
+      target_id = self.send(through_foreign_key)
+      results = DBConnection.execute(<<-SQL, target_id)
+      SELECT
+        #{source_table}.*
+      FROM
+        #{through_table}
+      JOIN
+        #{source_table} ON #{through_table}.#{source_foreign_key} = #{source_table}.#{source_primary_key}
+      WHERE
+        #{through_table}.#{through_primary_key} = ?
+      SQL
+      source_class = source_options.model_class
+      source_class.parse_all(results).first
+    end
   end
 end
 
