10ae8403731307c8639405284a229958f9bfdd10
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-17 20:46:24 -0400

    Code refactor: reduce interface of has_one_through, hiding details in private methods

--- lib/associate.rb
+++ lib/associate.rb
@@ -61,29 +61,47 @@ module Associates
 
   def has_one_through(name, through_name, source_name)
     define_method(name) do
-      through_options = self.class.assoc_options[through_name]
-      source_options = through_options.model_class.assoc_options[source_name]
-      through_table = through_options.table_name
-      through_primary_key = through_options.primary_key
-      through_foreign_key = through_options.foreign_key
-      source_table = source_options.table_name
-      source_primary_key = source_options.primary_key
-      source_foreign_key = source_options.foreign_key
-
-      target_id = self.send(through_foreign_key)
-      results = DBConnection.execute(<<-SQL, target_id)
-      SELECT
-        #{source_table}.*
-      FROM
-        #{through_table}
-      JOIN
-        #{source_table} ON #{through_table}.#{source_foreign_key} = #{source_table}.#{source_primary_key}
-      WHERE
-        #{through_table}.#{through_primary_key} = ?
-      SQL
-      source_class = source_options.model_class
+      instance = self
+      tables_info = self.class.send(
+        :through_and_source, through_name, source_name
+      )
+      results = self.class.send(
+        :through_source_join, instance, tables_info.first, tables_info.last
+      )
+      source_name = source_name.to_s.singularize
+      source_class = source_name.camelcase.constantize
       source_class.parse_all(results).first
     end
+  end
+
+  private
+
+  def through_and_source(through_name, source_name)
+    through_options = self.assoc_options[through_name]
+    source_options = through_options.model_class.assoc_options[source_name]
+    table_info = []
+    [through_options, source_options].each do |options|
+      table_info << { name: options.table_name,
+                      p_key: options.primary_key,
+                      f_key: options.foreign_key }
+    end
+    table_info
+  end
+
+  def through_source_join(instance, through, source)
+    target_id = instance.send(through[:f_key])
+    DBConnection.execute(<<-SQL, target_id)
+    SELECT
+      #{source[:name]}.*
+    FROM
+      #{through[:name]}
+    JOIN
+      #{source[:name]}
+    ON
+      #{through[:name]}.#{source[:f_key]} = #{source[:name]}.#{source[:p_key]}
+    WHERE
+      #{through[:name]}.#{through[:p_key]} = ?
+    SQL
   end
 end
 
