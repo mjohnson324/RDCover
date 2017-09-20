4d3b9710f0465d8ca01b19e0ddd22501ea85f4f6
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-19 23:50:35 -0400

    Update demo, filling out required options

--- demo.rb
+++ demo.rb
@@ -4,20 +4,41 @@ ROOT_FOLDER = File.join(File.dirname(__FILE__), '/')
 SQL_FILE = File.join(ROOT_FOLDER, 'simpsons.sql')
 DB_FILE = File.join(ROOT_FOLDER, 'simpsons.db')
 DBConnection.open(DB_FILE)
+
 # pets: id, name, owner_id
 # characters, id, fname, lname, house_id
 # homes: id, address
 
 class Pet < DataCover
-  belongs_to :character
+  belongs_to :character,
+             class_name: "Character",
+             foreign_key: :owner_id,
+             primary_key: :id
+
   has_one_through :house, :character, :home
+
+  Pet.finalize!
 end
 
 class Character < DataCover
-  has_many :pets
-  belongs_to :home
+  has_many :pets,
+           class_name: "Pet",
+           foreign_key: :owner_id,
+           primary_key: :id
+
+  belongs_to :home,
+             class_name: "Home",
+             foreign_key: :house_id,
+             primary_key: :id
+
+  Character.finalize!
 end
 
 class Home < DataCover
-  has_many :characters
+  has_many :characters,
+           class_name: "Character",
+           foreign_key: :house_id,
+           primary_key: :id
+
+  Home.finalize!
 end
